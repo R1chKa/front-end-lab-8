4c085227d353155b897e4f0418e7cd121c576bcc
Volodymyr Vintonyak <v.vint7@gmail.com>
2018-02-10 15:05:39 +0200

    fix task2
    -add firstAttPrize value to get perfect attPrize after game continue;
    -delete useless code lines (winCounter, game)

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,9 +1,9 @@
 let prize = 0,
     range = 5,
     attPrize = 10;
-let startGame = confirm('Do you want to play a game ?');
 let reStart = true,
-    winCounter;
+    firstAttPrize;
+let startGame = confirm('Do you want to play a game ?');
 
 if (startGame == true) {
     while (reStart == true) {
@@ -14,22 +14,25 @@ if (startGame == true) {
         userNum = 0;
         reStart = true;
         while (i <= att) {
+            if (i == att) {
+                firstAttPrize = attPrize;
+                console.log(firstAttPrize);
+            }
 
             let randomNum = Math.floor(Math.random() * range) + 1;
             console.log(randomNum);
             let userNum = parseInt(prompt('Enter a number from 0 to ' + range +
-                '\nAttempts left: ' + (i) +
+                '\nAttempts left: ' + i +
                 '\nTotal prize: ' + prize + '$' +
                 '\nPossible prize on current attempt: ' + attPrize + '$'));
 
             if (userNum == randomNum) { // win
                 prize += attPrize;
-                winCounter++;
                 let continueGame = confirm('You are lucky! Do you want to continue game?');
                 if (continueGame == true) { // continue
                     reStart = true;
                     range = range * 2;
-                    attPrize = attPrize * 3;
+                    attPrize = firstAttPrize * 3;
                     break;
                 } else { // end
                     reStart = false;
@@ -47,9 +50,9 @@ if (startGame == true) {
                     if (tryAgain == true) {
                         reStart = true;
                         prize = 0;
+                        attPrize = 10;
                         att = 3;
                         i = 1;
-                        game = 'start';
                     } else { // end
                         reStart = false;
                         console.log('Thank you for a game. Come again');
