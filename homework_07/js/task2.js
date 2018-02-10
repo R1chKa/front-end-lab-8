284e5a7a58c8fa291da10f407718efcb12afa584
Volodymyr Vintonyak <v.vint7@gmail.com>
2018-02-10 15:13:21 +0200

    task 2 code refactoring

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -5,14 +5,15 @@ let reStart = true,
     firstAttPrize;
 let startGame = confirm('Do you want to play a game ?');
 
-if (startGame == true) {
-    while (reStart == true) {
+if (startGame) {
+    while (reStart) {
 
         // start
         let att = 3;
         let i = att;
         userNum = 0;
         reStart = true;
+
         while (i <= att) {
             if (i == att) {
                 firstAttPrize = attPrize;
@@ -29,7 +30,8 @@ if (startGame == true) {
             if (userNum == randomNum) { // win
                 prize += attPrize;
                 let continueGame = confirm('You are lucky! Do you want to continue game?');
-                if (continueGame == true) { // continue
+
+                if (continueGame) { // continue
                     reStart = true;
                     range = range * 2;
                     attPrize = firstAttPrize * 3;
@@ -39,21 +41,25 @@ if (startGame == true) {
                     console.log('Thank you for a game. Come again');
                     break;
                 }
+
             } else { // mistake
                 if (i > 1) {
                     i--;
                     attPrize = parseInt(attPrize / 2);
                     continue;
+
                 } else { // loose
                     console.log('Thank you for a game. Your prize is: ' + prize + '$');
                     let tryAgain = confirm('You are not lucky! Do you want to try again?');
-                    if (tryAgain == true) {
+
+                    if (tryAgain) {
                         reStart = true;
                         prize = 0;
                         attPrize = 10;
                         att = 3;
                         range = 5;
                         i = 1;
+
                     } else { // end
                         reStart = false;
                         console.log('Thank you for a game. Come again');
